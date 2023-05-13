8f6af12160a093972c1f524795a0d87afe7bc8bd
frckbrice <maebrie2017@gmail.com>
2023-05-13 18:12:58 +0100

    fix binding dom

---
+++ assets/scripts/isomorphic.js
@@ -0,0 +1,42 @@
+//* bind html dom elements
+const show = document.querySelector(".display");
+const check = document.querySelector(".btn");
+check.addEventListener("click", display);
+check.addEventListener("keyup", (e) => {
+  e.preventDefault();
+  if (e.keyCode === 13) {
+    display();
+  }
+});
+
+//* function to display result of checking prcess
+function display() {
+  const str1 = document.querySelector("#str1").value;
+  const str2 = document.querySelector("#str2").value;
+
+  show.innerHTML = isIsomorphic(str1, str2);
+}
+
+//*function to check if two string are isomorphic
+
+const isIsomorphic = (str1, str2) => {
+  //checking first if the two strings are the same length. if not nothing to search
+  if (str1.length !== str2.length) {
+    return "Strings are not the same length. Please input Strings with the same length";
+  }
+  for (let i = 0; i < str1.length; i++) {
+    /* run trough each string and looking for the mapping between characters of both strings. we use index intead of character to avoid using map() method */
+    let ind1 = str1.indexOf(str1[i]);
+    let ind2 = str2.indexOf(str2[i]);
+    if (str2[ind1] !== str2[i] || str1[ind2] !== str1[i]) {
+      return "FALSE! Thes Strings are  not Isomorphic Strings";
+    }
+  }
+  return "TRUE! Thes Strings are Isomorphic Strings";
+};
+
+// let str1 = "egg";
+// let str2 = "add";
+// console.log("checking for  " + str1 + "  and  " + str2);
+// console.log();
+// console.log(isIsomorphic(str1, str2));
