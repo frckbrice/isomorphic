bcced63af52c9366c06a7b5124aedd8d62ed14af
frckbrice <maebrie2017@gmail.com>
2023-08-16 12:04:35 +0100

    customizing ui

--- assets/scripts/isomorphic.js
+++ assets/scripts/isomorphic.js
@@ -11,8 +11,8 @@ check.addEventListener("keyup", (e) => {
 
 //* function to display result of checking prcess
 function display() {
-  const str1 = document.querySelector("#str1").value;
-  const str2 = document.querySelector("#str2").value;
+  const str1 = document.querySelector("#str1").value.trim();
+  const str2 = document.querySelector("#str2").value.trim();
 
   // basic validation
   if(str1.length == 0 || str2.length == 0) {
@@ -27,17 +27,17 @@ function display() {
 const isIsomorphic = (str1, str2) => {
   //checking first if the two strings are the same length. if not nothing to search
   if (str1.length !== str2.length) {
-    return "Strings are not the same length. Please input Strings with the same length";
+    return "Strings are not the same length. Please inputs Strings with the same length";
   }
   for (let i = 0; i < str1.length; i++) {
-    /* run trough each string and looking for the mapping between characters of both strings. we use index intead of character to avoid using map() method */
+    /* run trough each string looking for the mapping between characters of both strings. we use index intead of character to avoid using map() method */
     let ind1 = str1.indexOf(str1[i]);
     let ind2 = str2.indexOf(str2[i]);
     if (str2[ind1] !== str2[i] || str1[ind2] !== str1[i]) {
-      return `<strong>FALSE!</strong> <span>Thes Strings ${str1} and ${str2} are  not Isomorphic Strings<span>`;
+      return `<strong>FALSE!</strong><br/> <span>&nbsp;&nbsp;Thes Strings ${str1} and ${str2} are  not Isomorphic Strings<span>`;
     }
   }
-  return `<div class= 'to-show'><strong>TRUE!</strong> <span>Thes Strings ${str1} and ${str2} are Isomorphic Strings<span></div>`;
+  return `<div class= 'to-show'><strong>TRUE!</strong><br/> <span>Thes Strings ${str1} and ${str2} are Isomorphic Strings<span></div>`;
 };
 
 // let str1 = "egg";
